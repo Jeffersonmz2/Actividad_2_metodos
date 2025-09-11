eb4730a1ce876f842f78ee11244fb5542b3bbb27
Jeffersonmz2 <Jeffersonmz2@users.noreply.github.com>
2025-09-11 17:19:11 -0500

    Problema 1: Estimación de la probabilidad y la media Cálculo de probabilidad teórica: cuando x=3

--- scrip_actividad_2.R
+++ scrip_actividad_2.R
@@ -1,5 +1,6 @@
+#Problema 1: Estimación de la probabilidad y la media
 
-
+# a. Cálculo de probabilidad teórica:
 # x = 3
-(dpois(3,5))
-
+xes3 <- dpois(3,5)
+print(paste0("La probabilidad de que en una hora lleguen exactamente 3 es ", xes3))
