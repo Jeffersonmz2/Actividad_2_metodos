fc58c398155e2c50ba61c8f2e2c1e6b0924465b9
Rick2898 <ricardovargasgarzon@gmail.com>
2025-09-14 18:35:11 -0500

    prueba_git_funcionando

--- scrip_actividad_2.R
+++ scrip_actividad_2.R
@@ -3,4 +3,7 @@
 # a. Cálculo de probabilidad teórica:
 # x = 3
 xes3 <- dpois(3,5)
-print(paste0("La probabilidad de que en una hora lleguen exactamente 3 es ", xes3))
+print(paste0("La probabilidad de que en una hora lleguen exactamente 3 es ", xes3)
+      
+      
+      ##prueba de como funciona git
