c8068c1d4f25e0ef965ecbb18bab5d9f8aa03055
Jeffersonmz2 <Jeffersonmz2@users.noreply.github.com>
2025-09-14 21:40:52 -0500

    prueba

--- scrip_actividad_2.R
+++ scrip_actividad_2.R
@@ -1,6 +1,50 @@
+set.seed(123)
+
 #Problema 1: Estimación de la probabilidad y la media
 
 # a. Cálculo de probabilidad teórica:
 # x = 3
 xes3 <- dpois(3,5)
-print(paste0("La probabilidad de que en una hora lleguen exactamente 3 es ", xes3))
+
+n=1000
+
+muestra <- rpois(n, lambda = 5)
+# x=3
+# 2. Calcular la frecuencia absoluta para X=3
+frecuencia_absoluta <- sum(muestra == 3)
+frecuencia_relativa <- frecuencia_absoluta / n
+
+#Compara e interpreta el resultado frente a la probabilidad teórica.
+
+(frecuencia_relativa - xes3)
+
+
+#C. Análisis de la variabilidad entre muestras:
+
+#Genera 100 muestras aleatorias de tamaño n=1,000.
+#Calcula la frecuencia relativa para X=3 en cada muestra.
+#Construye un gráfico de dispersión:
+
+#Inicializar un vector vacío para guardar las frecuencias relativas
+frecuencias_relativas <- c()
+
+# Generar 100 muestras y calcular la frecuencia relativa para cada una
+for (i in 1:100) {
+  # Generar una muestra aleatoria de tamaño 1000 con lambda = 5
+  muestra <- rpois(1000, lambda = 5)
+  
+  # Calcular la frecuencia de X=3
+  frecuencia_x_3 <- sum(muestra == 3)
+  
+  # Calcular la frecuencia relativa y guardarla
+  frecuencia_relativa_x_3 <- frecuencia_x_3 / 1000
+  frecuencias_relativas <- c(frecuencias_relativas, frecuencia_relativa_x_3)
+}
+
+simulaciones <- 1:100
+datos_grafico <- data.frame(Simulacion = simulaciones, Frecuencia = frecuencias_relativas)
+
+
+
+
+
